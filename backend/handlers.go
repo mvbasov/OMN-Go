b2d8880aa638cb7395aab3fa39118bc322c420bd
Mikhail Basov (NeedleFile) <mikhail[at]basov[dot]net>
2026-06-26 20:05:18 +0300

    fix(go): Remove unused import

--- backend/handlers.go
+++ backend/handlers.go
@@ -22,7 +22,6 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport"
-	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
 func getConfigPageBody() string {
